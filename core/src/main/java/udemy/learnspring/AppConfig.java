6c8257751183a50e8a7ad655a3b1ff9107b76055
lalantha-jay <lalantha.jayasuriya@gmail.com>
2019-08-28 16:03:32 -0700

    Modified to use Bean methods instead of annotation.

--- core/src/main/java/udemy/learnspring/AppConfig.java
+++ core/src/main/java/udemy/learnspring/AppConfig.java
@@ -1,11 +1,25 @@
 package udemy.learnspring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "udemy.learnspring")
+@ComponentScan(basePackages = "udemy.learnspring") // use bean scanning method
 public class AppConfig {
 
+    /*  Bean methods can be used instead of Annotations, if additional
+        configuration is requred for the bean
+    */
+    // == Bean methods ==
+    @Bean
+    public NumberGenerator numberGenerator() {
+        return new NumberGeneratorImpl();
+    }
+
+    @Bean
+    public Game game() {
+        return new GameImpl();
+    }
 
 }
