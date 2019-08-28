b07d629747d6c3d9cf70d48053c36c2ba44f83b5
lalantha-jay <lalantha.jayasuriya@gmail.com>
2019-08-28 15:49:41 -0700

    Added app config class for ComponentScan using Annotations.

---
+++ core/src/main/java/udemy/learnspring/AppConfig.java
@@ -0,0 +1,11 @@
+package udemy.learnspring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "udemy.learnspring")
+public class AppConfig {
+
+
+}
