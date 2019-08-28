b2f481fc4e8c07f4a32ed744a3a3a8d5d2b356e9
lalantha-jay <lalantha.jayasuriya@gmail.com>
2019-08-28 15:51:38 -0700

    Modified to use app config class and not XML config file location.

--- core/src/main/java/udemy/learnspring/Main.java
+++ core/src/main/java/udemy/learnspring/Main.java
@@ -3,24 +3,32 @@ package udemy.learnspring;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    private static final String CONFIG_LOCATION = "beans.xml";
+    // private static final String CONFIG_LOCATION = "beans.xml";
     public static void main(String[] args) {
         log.info("Guess The Number Game");
 
-        // create the context (container)
+        // create the context (container), if using XML
+        /*
         ConfigurableApplicationContext context
                 = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        */
+
+        // create the context (container) from configuration class
+        ConfigurableApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+
 
         // get number generator bean from context (container),
         // via overloading
         NumberGenerator numberGenerator
-                = context.getBean("numberGenerator", NumberGenerator.class);
+                = context.getBean(NumberGenerator.class);
 
         // call method next() to get a random number
         int num = numberGenerator.next();
