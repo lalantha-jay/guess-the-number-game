d525350dca95c9f9fe1f508d156ba63956028d5a
lalantha-jay <lalantha.jayasuriya@gmail.com>
2019-08-28 21:21:39 -0700

    Added implementation logic and messages for user.

--- core/src/main/java/udemy/learnspring/MessageGeneratorImpl.java
+++ core/src/main/java/udemy/learnspring/MessageGeneratorImpl.java
@@ -24,11 +24,29 @@ public class MessageGeneratorImpl implements MessageGenerator {
     // == public methods ==
     @Override
     public String getMainMessage(){
-        return ("getMainMessage() method called");
+        return "Number is between " +
+                game.getSmallest() +
+                " and " +
+                game.getBiggest() +
+                ". Can you guess it?";
     }
 
     @Override
     public String getResultMessage() {
-        return ("getResultMessage() method called");
+        if (game.isGameWon()) {
+            return "You guessed it! the number was " + game.getNumber();
+        } else if (game.isGameLost()){
+            return "You Lost.  The number was " + game.getNumber();
+        } else if (!game.isValidNumberRange()){
+            return "Invalid number range!";
+        } else if (game.getRemainingGuesses() == guessCount) {
+            return "What is your first guess?";
+        } else {
+            String direction = "Lower";
+            if (game.getGuess() < game.getNumber()) {
+                direction = "Higher";
+            }
+            return direction + "! You have " + game.getRemainingGuesses() + " guesses left" ;
+        }
     }
 }
