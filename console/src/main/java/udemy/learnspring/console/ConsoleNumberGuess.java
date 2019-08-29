29fddc795710e0f6eac515163a11a1c114fe41c6
lalantha-jay <lalantha.jayasuriya@gmail.com>
2019-08-29 02:41:10 -0700

    Added game logic to work in a user input loop.

--- console/src/main/java/udemy/learnspring/console/ConsoleNumberGuess.java
+++ console/src/main/java/udemy/learnspring/console/ConsoleNumberGuess.java
@@ -2,16 +2,27 @@ package udemy.learnspring.console;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import udemy.learnspring.Game;
+import udemy.learnspring.MessageGenerator;
+
+import java.util.Scanner;
 
 // Using Annotations to Handle Events
 @Component
 public class ConsoleNumberGuess {
     // == constants ==
     private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
+
+    // == fields ==
+    @Autowired
+    private Game game;
+    @Autowired
+    private MessageGenerator messageGenerator;
 
     //@EventListener
     // With annotations you can rename the method, here as "start"
@@ -23,8 +34,31 @@ public class ConsoleNumberGuess {
     // Therefore rather than using a parameter that is not used,
     // we can add an Event Type to the EventListener annotation.
     @EventListener(ContextRefreshedEvent.class)
-    public void start(ContextRefreshedEvent contextRefreshedEvent) {
+    public void start() {
         log.info("start() --> Container ready for use.");
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.println(messageGenerator.getMainMessage());
+            System.out.println(messageGenerator.getResultMessage());
+
+            int  guess = scanner.nextInt();
+            scanner.nextLine();
+            game.setGuess(guess);
+            game.check();
+
+            if(game.isGameWon() || game.isGameLost()) {
+                System.out.println(messageGenerator.getResultMessage());
+                System.out.println("Play again y/n?");
+
+                String playAgainString = scanner.nextLine().trim();
+                if(!playAgainString.equalsIgnoreCase("y")) {
+                    break;
+                }
+
+                game.reset();
+            }
+        }
     }
 }
 
