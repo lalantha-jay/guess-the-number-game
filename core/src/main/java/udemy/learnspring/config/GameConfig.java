e5ce40820ed33a9d3fa74b107418bc11fafd50c4
lalantha-jay <lalantha.jayasuriya@gmail.com>
2019-08-29 03:22:31 -0700

    Created a config file for Game application specific configuration custom annotations.

--- core/src/main/java/udemy/learnspring/config/GameConfig.java
+++ core/src/main/java/udemy/learnspring/config/GameConfig.java
@@ -1,4 +1,29 @@
 package udemy.learnspring.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import udemy.learnspring.GuessCount;
+import udemy.learnspring.MaxNumber;
+
+@Configuration
 public class GameConfig {
+
+    // == fields ==
+    private int maxNumber = 25;
+    private int guessCount = 8;
+
+    // == bean methods ==
+    @Bean
+    @MaxNumber
+    public int maxNumber() {
+        return maxNumber;
+    }
+
+    @Bean
+    @GuessCount
+    public int guessCount() {
+        return guessCount;
+    }
+
 }
