358de90a89a435fb90ef8cbff1cdd088271d7c84
lalantha-jay <lalantha.jayasuriya@gmail.com>
2019-08-28 21:55:10 -0700

    Modified event handling via EventListner annotation accepting an event type.

--- console/src/main/java/udemy/learnspring/console/ConsoleNumberGuess.java
+++ console/src/main/java/udemy/learnspring/console/ConsoleNumberGuess.java
@@ -4,8 +4,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+// Using Annotations to Handle Events
+@Component
+public class ConsoleNumberGuess {
+    // == constants ==
+    private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
+
+    //@EventListener
+    // With annotations you can rename the method, here as "start"
+    /* However ContextRefreshedEvent contextRefreshedEvent is not being used here
+    public void start(ContextRefreshedEvent contextRefreshedEvent) {
+        log.info("start() --> Container ready for use.");
+    }
+    */
+    // Therefore rather than using a parameter that is not used,
+    // we can add an Event Type to the EventListener annotation.
+    @EventListener(ContextRefreshedEvent.class)
+    public void start(ContextRefreshedEvent contextRefreshedEvent) {
+        log.info("start() --> Container ready for use.");
+    }
+}
+
+/* Another older method is to implement the ApplicationListener
 @Component
 public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
     // == constants ==
@@ -16,3 +39,4 @@ public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedE
         log.info("Container ready for use.");
     }
 }
+*/
