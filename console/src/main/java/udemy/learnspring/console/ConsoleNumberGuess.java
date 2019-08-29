323dd299e5ff6a42f058f9dd7f57ebbead57ab09
lalantha-jay <lalantha.jayasuriya@gmail.com>
2019-08-28 21:33:29 -0700

    Added ApplicationListener for ContextRefreshedEvent, for event handling.

--- console/src/main/java/udemy/learnspring/console/ConsoleNumberGuess.java
+++ console/src/main/java/udemy/learnspring/console/ConsoleNumberGuess.java
@@ -1,4 +1,18 @@
 package udemy.learnspring.console;
 
-public class ConsoleNumberGuess {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
+    // == constants ==
+    private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        log.info("Container ready for use.");
+    }
 }
