52094dc67da34c3412a1e406ef77739ea00255ab
lalantha-jay <lalantha.jayasuriya@gmail.com>
2019-08-29 03:53:40 -0700

    Created a config file for Game application specific configuration custom annotations.

--- core/src/main/java/udemy/learnspring/config/GameConfig.java
+++ core/src/main/java/udemy/learnspring/config/GameConfig.java
@@ -1,17 +1,27 @@
 package udemy.learnspring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import udemy.learnspring.GuessCount;
 import udemy.learnspring.MaxNumber;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // == fields ==
-    private int maxNumber = 25;
-    private int guessCount = 8;
+
+    /* Hardcoded declaration now handled by addition
+    of a properties file, and included in GameConfig.
+     */
+    @Value("${game.maxNumber}")
+    private int maxNumber;
+
+    @Value("${game.guessCount}")
+    private int guessCount;
 
     // == bean methods ==
     @Bean
